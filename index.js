a1eb5c7364098c341956a7c1d2b70bb45c27bbbd
Ishh <88816438+ishh-xd@users.noreply.github.com>
2023-04-23 07:44:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ embeds:[
 new EmbedBuilder()
   .setTitle("Error")
   .setColor(`#ff0080`)
-.setDescription("Reverb is not working properly. My owner's account was termed by discord and is no more.\nWe've brought up a new music bot for you all. **MoE** the perfect music bot for you. [Click here to invite/add MoE in your server.]()")
+.setDescription("Reverb is not working properly. My owner's account was termed by discord and is no more.\nWe've brought up a new music bot for you all. **MoE** the perfect music bot for you. [Click here to invite/add MoE in your server.](https://discord.com/oauth2/authorize?client_id=1074176927247630356&permissions=8&scope=bot)")
 ]
 })
 })
