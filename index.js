aa8a43cfb2723f9a6af7aec2fb69385069d8d2c9
Ishh <88816438+ishh-xd@users.noreply.github.com>
2023-04-23 07:41:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const {
+Client,
+Collection,
+GatewayIntentBits,
+  ButtonBuilder,
+ ButtonStyle,
+   EmbedBuilder,ActionRowBuilder,
+  Partials,
+} = require("discord.js");
+
+const client = new Client({intents: [
+  GatewayIntentBits.Guilds], shards: "auto"})
+
+
+
+
+client.on("ready",()=>{
+console.log("💀✨🥀")
+})
+
+client.on("interactionCreate",async(interaction)=>{
+await interaction?.deferReply()
+await interaction?.channel?.send({
+embeds:[
+new EmbedBuilder()
+  .setTitle("Error")
+  .setColor(`#ff0080`)
+.setDescription("Reverb is not working properly. My owner's account was termed by discord and is no more.\nWe've brought up a new music bot for you all. **MoE** the perfect music bot for you. [Click here to invite/add MoE in your server.]()")
+]
+})
+})
+
+client.login(process.env.token)
